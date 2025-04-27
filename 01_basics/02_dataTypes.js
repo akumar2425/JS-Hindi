ef2c1c3726c414b2953fdd22d67aa2fa3360910c
Arvind Kumar <140153730+akumar2425@users.noreply.github.com>
2025-04-27 20:05:46 +0000

    new

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strice";
+
+// alert(3+3)  // we are using node JS, not browser
+
+console.log("Arvind Sharma")
+
+
+let age = 26
+let name = "Arvind Sharma"
+let isMarried = false
+
+// ............RANGE or dataTypes...................
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false or 0/1
+// null => standalone value
+// undefined => 
+// symbol
+
+
+// object
+
+
+
+// ...................type of....................
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
+
+
+
+
+
