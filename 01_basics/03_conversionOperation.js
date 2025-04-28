0bea2c8a9ff3b2acbc3cae673a7e5a92aebbbe95
Arvind Kumar <140153730+akumar2425@users.noreply.github.com>
2025-04-28 05:27:07 +0000

    new

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let name = "Arvind Sharma"
+// console.log(typeof(name))
+
+// let nameInNumber = Number(name);
+// console.log(typeof(nameInNumber))
+// console.log(name)
+
+
+// let score = 26
+// console.log(score)
+// console.log(typeof(score))
+
+// let scoreInString = String(score)
+// console.log(scoreInString)
+// console.log(typeof(scoreInString))
+
+
+// "33" => 33
+// "Arvind" => NaN
+// true => 1
+// false => 0
+
+
+// *********************   Operation  ***************************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**3)
+console.log(2%3)
+
+
+
